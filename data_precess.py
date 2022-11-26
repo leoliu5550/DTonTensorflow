8654f4539d2ec24862055f1dd4983d0f9ae70e5e
Leo <leoliu5550@gmail.com>
2022-11-26 13:58:29 +0800

    add test01

--- data_precess.py
+++ data_precess.py
@@ -53,71 +53,7 @@ def process_image(file_path):
     bbox_list = tf.convert_to_tensor(np.array([labels, x, y, w, h] ))
     return img, bbox_list
 
-import tensorflow as tf
-import cv2
-import os
-import numpy as np
 
-# dataset = tf.data.Dataset.list_files('./data/train/images/*', shuffle=False)
-class_names = ['cat', 'deer', 'dog', 'fox', 'person', 'rabbit', 'raccoon']
-"""
-class_names2 = {
-    0:'cat',
-    1:'deer', 
-    2:'dog', 
-    3:'fox',
-    4:'person',
-    5:'rabbit',
-    6:'raccoon'}
-"""
-IMG_PATH =["data/train/images","data/valid/images","data/test/images"]
-#create TFRECORD 
-
-def get_label_path(img_file_path):
-    path = b'labels'
-    # get file id like '0a2c3fdb-6788-4217-9a3e-fa1c9fbb18a4-dog_jpg.rf.9509acdcace6ac82c679652c058d168f'
-    parts = tf.strings.split(img_file_path, os.path.sep)[-1]
-    parts = tf.strings.split(parts,'.jpg')[-2]
-    # get parents folder brfore id
-    sple = tf.strings.split(img_file_path, os.path.sep)[:-2]
-    parents=b''
-    for i in range(sple.shape[0]):
-        parents = tf.io.gfile.join(parents,sple[i].numpy())
-    label_path = tf.strings.join([parts,b'.txt'])
-    label_path = tf.io.gfile.join(parents,path,label_path.numpy())
-    return label_path
-# fixed this 
-def process_image(file_path):
-    img = tf.io.read_file(file_path) # load the raw data from the file as a string
-    img = tf.image.decode_jpeg(img)
-    label_path = get_label_path(file_path)
-    label = tf.io.read_file(label_path)
-
-
-
-    label = tf.strings.split(label, sep="\n", maxsplit=-1)
-    label = tf.strings.split(label, sep=" ", maxsplit=-1).to_tensor(default_value='')
-    label = tf.strings.to_number(label,out_type=tf.dtypes.float32)
-    return img, label
-
-
-# should be start here
-file_path = "data/train/images/0a480c46-4831-43ba-a5fc-08258c2b6f55-dog_person_jpg.rf.3d6102e59c01a16c9daf26977330e5d2.jpg"
-label_path = get_label_path(file_path)
-with open(label_path, 'r') as bboxfile:
-    records = bboxfile.readlines()
-    for record in records:
-        fields = record.strip().split(',')
-        print(fields)
-        # filename = fields[0][:-4]
-        # labels = [labels_dict[x] for x in fields[1].split(';')]
-        # xmin = [float(x) for x in fields[2].split(';')]
-        # ymin = [float(x) for x in fields[3].split(';')]
-        # xmax = [float(x) for x in fields[4].split(';')]
-        # ymax = [float(x) for x in fields[5].split(';')]
-        # bbox_list[filename] = [labels, xmin, ymin, xmax, ymax] 
-# label = tf.io.read_file(label_path)
-# print(label)
 
 # dataset = dataset.map(process_image)
 
